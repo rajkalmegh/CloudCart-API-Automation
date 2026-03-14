45b101f138aac42565bfdc3580ef36652fd70166
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-03-14 20:36:16 +0530

    Update token_manager.py

--- automation_tests/token_manager.py
+++ automation_tests/token_manager.py
@@ -1,1 +1,16 @@
 
+from utils.api_client import APIClient
+
+client = APIClient()
+
+def get_token():
+
+    response = client.post(
+        "/login",
+        {
+            "username": "testuser",
+            "password": "123456"
+        }
+    )
+
+    return response.json()["token"]
