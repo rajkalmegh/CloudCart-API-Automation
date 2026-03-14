bb1dcfdff75b6a68a952b4f8cd9bfc2117b3d070
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-03-14 20:38:18 +0530

    Update test_orders.py

--- automation_tests/test.data/test_orders.py
+++ automation_tests/test.data/test_orders.py
@@ -1,1 +1,11 @@
 
+def test_create_order(api_client):
+
+    data = {
+        "product_id":"123",
+        "quantity":2
+    }
+
+    response = api_client.post("/orders", data)
+
+    assert response.status_code == 200
