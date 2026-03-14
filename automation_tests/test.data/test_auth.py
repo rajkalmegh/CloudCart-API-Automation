82b90b4808b2e83f4c364fc8672fa8639895aa43
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-03-14 20:37:48 +0530

    Update test_auth.py

--- automation_tests/test.data/test_auth.py
+++ automation_tests/test.data/test_auth.py
@@ -1,1 +1,36 @@
 
+def test_register_user(api_client):
+
+    data = {
+        "username":"testuser",
+        "password":"123456"
+    }
+
+    response = api_client.post("/register", data)
+
+    assert response.status_code == 200
+
+
+def test_login_user(api_client):
+
+    data = {
+        "username":"testuser",
+        "password":"123456"
+    }
+
+    response = api_client.post("/login", data)
+
+    assert response.status_code == 200
+    assert "token" in response.json()
+
+
+def test_login_invalid_password(api_client):
+
+    data = {
+        "username":"testuser",
+        "password":"wrong"
+    }
+
+    response = api_client.post("/login", data)
+
+    assert response.status_code == 401
