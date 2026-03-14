9227d0c45e1e0b440133f9358189d8a57162df89
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-03-14 20:38:06 +0530

    Update test_products.py

--- automation_tests/test.data/test_products.py
+++ automation_tests/test.data/test_products.py
@@ -1,1 +1,18 @@
 
+def test_create_product(api_client):
+
+    data = {
+        "name":"Phone",
+        "price":20000
+    }
+
+    response = api_client.post("/products", data)
+
+    assert response.status_code == 200
+
+
+def test_get_products(api_client):
+
+    response = api_client.get("/products")
+
+    assert response.status_code == 200
