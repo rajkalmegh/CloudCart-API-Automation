ea4d9dc6aab147207013690737abcf2e351f1337
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-03-14 23:24:41 +0530

    Update api_client.py

--- automation_tests/utils/api_client.py
+++ automation_tests/utils/api_client.py
@@ -1,12 +1,23 @@
-import urllib3
-urllib3.disable_warnings()
 import requests
+import urllib3
 from utils.config import BASE_URL
+
+# Disable SSL warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class APIClient:
 
     def get(self, endpoint, headers=None):
-        requests.get(BASE_URL + endpoint, headers=headers, verify=False)
+        return requests.get(
+            BASE_URL + endpoint,
+            headers=headers,
+            verify=False
+        )
 
     def post(self, endpoint, data=None, headers=None):
-        requests.post(BASE_URL + endpoint, json=data, headers=headers, verify=False)
+        return requests.post(
+            BASE_URL + endpoint,
+            json=data,
+            headers=headers,
+            verify=False
+        )
