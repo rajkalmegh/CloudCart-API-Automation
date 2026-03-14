a9f1b98ff346f6d5337ebb44ee959e3d3c64ed2a
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-03-14 20:27:25 +0530

    Update api_client.py

--- automation_tests/utils/api_client.py
+++ automation_tests/utils/api_client.py
@@ -1,1 +1,11 @@
 
+import requests
+from utils.config import BASE_URL
+
+class APIClient:
+
+    def get(self, endpoint, headers=None):
+        return requests.get(BASE_URL + endpoint, headers=headers)
+
+    def post(self, endpoint, data=None, headers=None):
+        return requests.post(BASE_URL + endpoint, json=data, headers=headers)
